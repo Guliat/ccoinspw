e677f8ef2fdce4a7137f2d23d0f6ee9a2748838e
AleksandarEnchev <guliat88@mail.bg>
2020-12-01 13:13:22 +0200

    trades seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        for ($i = 0; $i < 150; $i++) {
+            DB::table('trades')->insert([
+                'user_id' => 4,
+                'exchange_id' => rand(1, 5),
+                'coin_id' => rand(1, 20),
+                'quantity' => rand(1, 100),
+                'open_price' => rand(1, 15000),
+            ]);
+        }
     }
 }
