e2dc9ac0cbb3c73df0cb453e9127db3d847d8d09
AleksandarEnchev <guliat88@mail.bg>
2020-12-23 23:02:22 +0200

    seeders fix

--- database/seeders/TradesSeeder.php
+++ database/seeders/TradesSeeder.php
@@ -17,26 +17,26 @@ class TradesSeeder extends Seeder
 
         for ($i = 1; $i < 6; $i++) {
             DB::table('exchange_user')->insert([
-                'user_id' => 4,
+                'user_id' => 1,
                 'exchange_id' => $i,
             ]);
         }
 
         for ($i = 1; $i < 11; $i++) {
             DB::table('coin_user')->insert([
-                'user_id' => 4,
+                'user_id' => 1,
                 'coin_id' => $i,
             ]);
         }
 
 
-        for ($i = 0; $i < 150; $i++) {
+        for ($i = 0; $i < 50; $i++) {
             DB::table('trades')->insert([
-                'user_id' => 4,
+                'user_id' => 1,
                 'exchange_id' => rand(1, 5),
                 'coin_id' => rand(1, 10),
                 'quantity' => rand(1, 100),
-                'open_price' => rand(1, 15000),
+                'open_price' => rand(1, 5000),
             ]);
         }
 
