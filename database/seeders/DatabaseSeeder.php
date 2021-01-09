c0f417899f0d9784682151bcd44f1bbe66041b7b
AleksandarEnchev <guliat88@mail.bg>
2021-01-09 20:20:07 +0200

    seeder - create user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +17,18 @@ class DatabaseSeeder extends Seeder
    */
   public function run()
   {
+    
+    DB::table('users')->insert([
+      'name' => Str::random(10),
+      'email' => Str::random(10) . '@gmail.com',
+      'password' => Hash::make('password'),
+    ]);
+
     $this->call([
       CoinsSeeder::class,
       ExchangesSeeder::class,
       TradesSeeder::class,
     ]);
+
   }
 }
